fc05724fd9abe62fdc317673938a173044b299b1
古欣以 <m10721229@gemail.yuntech.edu.tw>
2019-02-21 21:00:19 +0800

    Set

---
+++ ClassP04_Set.py
@@ -0,0 +1,6 @@
+allStudents={'John','Eva','Jill','Eric','Andy','Albert','Polina','Kristin','Angela'}
+danceClub={'John','Eva','Jill','Eric','Andy'}
+guitarClub={'Andy','Eric','Albert','Polina','Kristin'}
+print(danceClub&guitarClub)
+print(guitarClub-danceClub)
+print(allStudents-danceClub-guitarClub)
