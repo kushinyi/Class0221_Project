6f6b9b7ee60b41284b8c83f27ead10e995bcdb2f
古欣以 <m10721229@gemail.yuntech.edu.tw>
2019-02-21 20:48:53 +0800

    main

---
+++ ClassP03_dictionary.py
@@ -0,0 +1,6 @@
+gradesDict={'chinese':[5,14,7],'english':[23,36,28],'math':[88,80,92]}
+print('mathgrades:',gradesDict['math'])
+ChineseScores=gradesDict['chinese']
+print('chineseAvg:',sum(ChineseScores)]/len(ChineseScores))
+#print('englishAvg:',gradesDict[sum('english')]/gradesDict[len('english')])
+#print('mathAvg:',gradesDict[sum('math')]/gradesDict[len('math')])
