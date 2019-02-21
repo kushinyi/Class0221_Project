50ad21454c1fe157224e4467a1bc9ac83e3d31ab
古欣以 <m10721229@gemail.yuntech.edu.tw>
2019-02-21 19:52:21 +0800

    tuple gradeschinese,math,english addend science

---
+++ ClassP02.py
@@ -0,0 +1,7 @@
+gradesTuple=((5,14,7),(23,36,28),(88,80,92))
+print('Mathgrades:',gradesTuple[2])
+print('AvgMath:',sum(gradesTuple[2])/len(gradesTuple[2]))
+print('AvgEnglish:',sum(gradesTuple[1])/len(gradesTuple[1]))
+print('AvgChinese:',sum(gradesTuple[0])/len(gradesTuple[0]))
+gradesScience=((94,90,96),)
+print(gradesTuple+gradesScience)
