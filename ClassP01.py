4b77eceb55614d3e7b58d1bf9826cc2972b48cd3
古欣以 <m10721229@gemail.yuntech.edu.tw>
2019-02-21 19:25:38 +0800

    gradeschinese,math,english addend science

---
+++ ClassP01.py
@@ -0,0 +1,7 @@
+grades=[[5,14,7],[23,36,28],[88,80,92]]
+print(grades[0])
+print(sum(grades[0])/len(grades[0])) #國文平均
+print(sum(grades[1])/len(grades[1])) #英文平均
+print(sum(grades[2])/len(grades[2])) #數學平均
+grades.append([94,90,96])
+print(grades)
